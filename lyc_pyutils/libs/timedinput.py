171c1ad9b49f20af144cd836e1590f6cea9d406e
liu-yucheng <liu.r.yucheng@gmail.com>
2023-10-18 15:20:26 +0800

    Updated Python 3.11.6 support

--- lyc_pyutils/libs/timedinput.py
+++ lyc_pyutils/libs/timedinput.py
@@ -1,6 +1,6 @@
 """Timed input."""
 
-# Copyright 2022 Yucheng Liu. GNU LGPL3 license.
+# Copyright 2022-2023 Yucheng Liu. GNU LGPL3 license.
 # GNU LGPL3 license copy: https://www.gnu.org/licenses/lgpl-3.0.txt
 # GNU LGPL3 is based on GNU GPL3, GNU GPL3 copy: https://www.gnu.org/licenses/gpl-3.0.txt
 # First added by username: liu-yucheng
@@ -25,7 +25,7 @@ class TimedInput:
     """
 
     # Part of LYC-PythonUtils
-    # Copyright 2022 Yucheng Liu. GNU LGPL3 license.
+    # Copyright 2022-2023 Yucheng Liu. GNU LGPL3 license.
     # GNU LGPL3 license copy: https://www.gnu.org/licenses/lgpl-3.0.txt
     # GNU LGPL3 is based on GNU GPL3, GNU GPL3 copy: https://www.gnu.org/licenses/gpl-3.0.txt
 
--- lyc_pyutils/tests/test_jsonrw.py
+++ lyc_pyutils/tests/test_jsonrw.py
@@ -1,6 +1,6 @@
 """Executable that tests the json reading and writing utilities."""
 
-# Copyright 2022 Yucheng Liu. GNU LGPL3 license.
+# Copyright 2022-2023 Yucheng Liu. GNU LGPL3 license.
 # GNU LGPL3 license copy: https://www.gnu.org/licenses/lgpl-3.0.txt
 # GNU LGPL3 is based on GNU GPL3, GNU GPL3 copy: https://www.gnu.org/licenses/gpl-3.0.txt
 # First added by username: liu-yucheng
